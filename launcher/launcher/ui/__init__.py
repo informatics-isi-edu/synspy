d8347132e246caf4874384000014353ce200dff4
mikedarcy <mikedarcy@users.noreply.github.com>
2017-07-24 15:56:34 -0700

    Set prod server as config default.

--- launcher/launcher/ui/__init__.py
+++ launcher/launcher/ui/__init__.py
@@ -3,7 +3,7 @@ CURATORS = "https://auth.globus.org/6265343a-52e3-11e7-acd7-22000b100078"
 DEFAULT_CONFIG = {
   "server": {
     "protocol": "https",
-    "host": "",
+    "host": "synapse.isrd.isi.edu",
     "catalog_id": 1
   },
   "viewer_mode": "2d",

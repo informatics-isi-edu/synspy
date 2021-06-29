9e73851ea1b7607069e7facfb2ac9237c3369d2b
Karl Czajkowski <karlcz@isi.edu>
2021-06-29 12:20:01 -0700

    request chunked transfer when calling the hatrac store put_loc() method

--- synspy/worker.py
+++ synspy/worker.py
@@ -370,7 +370,8 @@ class Worker (object):
         return self.store.put_loc(
             '%s/ROI_%s.npz' % (self.subject_path, self.row['RID']),
             'ROI_%s.npz' % self.row['RID'],
-            headers={'Content-Type': 'application/octet-stream'}
+            headers={'Content-Type': 'application/octet-stream'},
+            chunked=True,
         )
 
     def filter_synspy_csv(self, csv_url):
@@ -417,7 +418,8 @@ class Worker (object):
         return self.store.put_loc(
             '%s/%s' % (self.subject_path, segments_filtered_file),
             segments_filtered_file,
-            headers={'Content-Type': 'text/csv'}
+            headers={'Content-Type': 'text/csv'},
+            chunked=True,
         )
 
     def compute_synspy_stats(self, csv_url, existing_row={}):
@@ -467,12 +469,14 @@ class Worker (object):
         n1_url = self.store.put_loc(
             '%s/%s' % (self.subject_path, n1_outfile),
             n1_outfile,
-            headers={'Content-Type': 'text/csv'}
+            headers={'Content-Type': 'text/csv'},
+            chunked=True,
         )
         n2_url = self.store.put_loc(
             '%s/%s' % (self.subject_path, n2_outfile),
             n2_outfile,
-            headers={'Content-Type': 'text/csv'}
+            headers={'Content-Type': 'text/csv'},
+            chunked=True,
         )
         return M, n1_url, n2_url
 
@@ -506,12 +510,14 @@ class Worker (object):
         s1_url = self.store.put_loc(
             '%s/%s' % (self.subject_path, s1_outfile),
             s1_outfile,
-            headers={'Content-Type': 'text/csv'}
+            headers={'Content-Type': 'text/csv'},
+            chunked=True,
         )
         s2_url = self.store.put_loc(
             '%s/%s' % (self.subject_path, s2_outfile),
             s2_outfile,
-            headers={'Content-Type': 'text/csv'}
+            headers={'Content-Type': 'text/csv'},
+            chunked=True,
         )
         return s1_url, s2_url
 
